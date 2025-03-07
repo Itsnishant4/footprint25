02fb682b690ec557822f6ad133faa644b59df05d
kunalpithva <161003832+kunalpithva@users.noreply.github.com>
2025-03-07 12:10:21 +0530

    main

--- client/footprint25/src/components/header.js
+++ client/footprint25/src/components/header.js
@@ -3,27 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        <h1>
-          Footprint25
-          sghhbddskodsjg
-        </h1>
-=======
-        <h1>dvbdbdf</h1>
->>>>>>> Stashed changes
-=======
-        <h1>dvbdbdf</h1>
->>>>>>> Stashed changes
-=======
-        <h1>dvbdbdf</h1>
->>>>>>> Stashed changes
-=======
-        <h1>dvbdbdf</h1>
->>>>>>> Stashed changes
-
+        <h1>Header</h1>
     </div>
   )
 }
