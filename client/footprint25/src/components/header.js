283011559fc659b1f43142d307531d426cdba2f2
kunalpithva <161003832+kunalpithva@users.noreply.github.com>
2025-03-07 12:08:43 +0530

    main

--- client/footprint25/src/components/header.js
+++ client/footprint25/src/components/header.js
@@ -4,10 +4,14 @@ function Header() {
   return (
     <div>
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         <h1>
           Footprint25
           sghhbddskodsjg
         </h1>
+=======
+        <h1>dvbdbdf</h1>
+>>>>>>> Stashed changes
 =======
         <h1>dvbdbdf</h1>
 >>>>>>> Stashed changes
