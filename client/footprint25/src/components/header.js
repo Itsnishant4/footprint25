ed99615ecc582a09150cc4c7e06e6ff9d3025139
Itsnishant4 <139362611+Itsnishant4@users.noreply.github.com>
2025-03-07 12:13:14 +0530

    Update header.js

--- client/footprint25/src/components/header.js
+++ client/footprint25/src/components/header.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Header() {
   return (
     <div>
-        <h1>Header</h1>
+        <h1 classname="header bg-gray-50">
+            footprint25
+        </h1>
 
     </div>
   )
